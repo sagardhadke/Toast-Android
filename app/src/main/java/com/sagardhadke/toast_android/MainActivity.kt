c9aa45914749977519e57be287765380d5636f41
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-29 22:40:13 +0530

    Default Toast

--- app/src/main/java/com/sagardhadke/toast_android/MainActivity.kt
+++ app/src/main/java/com/sagardhadke/toast_android/MainActivity.kt
@@ -2,10 +2,20 @@ package com.sagardhadke.toast_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        textView.setOnClickListener {
+            Toast.makeText(this, "Testing Default Toast", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
